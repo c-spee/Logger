1884ed96515fd8c3dd56e0a9d386fec4551a6727
Ciprian Spiridon <ciprian.spiridon@porsche-engineering.ro>
2021-12-17 09:28:56 +0200

    UT error

--- test/logger_test.cpp
+++ test/logger_test.cpp
@@ -6,7 +6,7 @@ TEST(HelloTest, BasicAssertions) {
 // Expect two strings not to be equal.
 EXPECT_STRNE("hello", "world");
 // Expect equality.
-EXPECT_EQ(7 * 6, 42);
+EXPECT_EQ(7 * 6, 41);
 // Expect inequality
 EXPECT_NE(-1, 0xFFFFFFF0);
 EXPECT_NE(7, 0xFFFFFFF0);
